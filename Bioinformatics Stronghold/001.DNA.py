ee12713306cab04ecfb55ed1a9630795bfdb0684
Qi Fang <secretloong@outlook.com>
2021-12-07 13:45:13 +0800

    Create 001.DNA.py
    001.DNA

---
+++ Bioinformatics Stronghold/001.DNA.py
@@ -0,0 +1,7 @@
+
+def CountingDNANucleotides():
+	dna='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+	print (dna.count('A'),dna.count('C'),dna.count('G'),dna.count('T'))
+
+if __name__ == '__main__':
+	CountingDNANucleotides()
