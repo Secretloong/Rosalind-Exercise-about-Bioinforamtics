1c6656882f9ef54202113b917c233bc94e48752a
Qi Fang <secretloong@outlook.com>
2021-12-30 15:25:05 +0800

    sum of geometric series

---
+++ Bioinformatics Stronghold/042.INOD.py
@@ -0,0 +1,11 @@
+
+def CountingPhylogeneticAncestors(n):
+	#sum of geometric series
+	#(a1-an*q)/(1-q), q=1/2, a1=n/2, an=2, unrooted
+	#rooted, an=1
+	print(n-2)
+	print(n-1)
+
+
+if __name__=='__main__':
+	CountingPhylogeneticAncestors(6130)
